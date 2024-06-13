cead77d854c5f81418768e7efab16728dfedbaed
AHMED ALBALAWI <102583986+rootAHMED@users.noreply.github.com>
2024-06-13 11:16:57 +0300

    Server.cpp

---
+++ Server.cpp
@@ -0,0 +1,63 @@
+// server.cpp
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 1234
+#define BUF_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUF_SIZE] = {0};
+    const char *hello = "Hello from server:";
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server listening on port... " << PORT << std::endl;
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    read(new_socket, buffer, BUF_SIZE);
+    std::cout << "Message received: " << buffer << std::endl;
+    send(new_socket, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
